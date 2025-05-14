42ac5312ec00428d18b67e4c5022db24f84d626c
JoeyShapiro <joeyashapiro@gmail.com>
2025-05-13 21:08:50 -0500

    basic stuff

--- ccode/Canary/src/main.cpp
+++ ccode/Canary/src/main.cpp
@@ -1,84 +1,44 @@
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_BME680.h"
+// Adafruit_ImageReader test for Adafruit E-Ink Breakouts.
+// Demonstrates loading images from SD card or flash memory to the screen,
+// to RAM, and how to query image file dimensions.
+// Requires BMP file in root directory of QSPI Flash:
+// blinka.bmp.
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
+#include <SdFat.h>					  // SD card & FAT filesystem library
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define EPD_CS 9	// can be any pin, but required!
+#define EPD_DC 10	// can be any pin, but required!
+#define SRAM_CS -1	// can set to -1 to not use a pin (uses a lot of RAM!)
+#define EPD_RESET 5 // can set to -1 and share with chip Reset (can't deep sleep)
+#define EPD_BUSY 6	// can set to -1 to not use a pin (will wait a fixed delay)
+#define SD_CS 11		// SD card chip select
 
-Adafruit_BME680 bme; // I2C
-//Adafruit_BME680 bme(BME_CS); // hardware SPI
-//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
+SdFat SD;							 // SD card filesystem
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println(F("BME680 async test"));
+void setup(void)
+{
+	Serial.begin(9600);
+	while(!Serial);           // Wait for Serial Monitor before continuing
 
-  if (!bme.begin()) {
-    Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
-    while (1);
-  }
-
-  // Set up oversampling and filter initialization
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_2X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150); // 320*C for 150 ms
+	// The Adafruit_ImageReader constructor call (above, before setup())
+	// accepts an uninitialized SdFat or FatVolume object. This MUST
+	// BE INITIALIZED before using any of the image reader functions!
+	Serial.print(F("Initializing filesystem..."));
+	// SPI or QSPI flash requires two steps, one to access the bare flash
+	// memory itself, then the second to access the filesystem within...
+	
+	// SD card is pretty straightforward, a single call...
+	if (!SD.begin(SD_CS, SD_SCK_MHZ(10)))
+	{ // Breakouts require 10 MHz limit due to longer wires
+		Serial.println(F("SD begin() failed"));
+		for (;;)
+			; // Fatal error, do not continue
+	}
+	
+	Serial.println(F("OK!"));
+	SD.ls(LS_SIZE);
 }
 
-void loop() {
-  // Tell BME680 to begin measurement.
-  unsigned long endTime = bme.beginReading();
-  if (endTime == 0) {
-    Serial.println(F("Failed to begin reading :("));
-    return;
-  }
-  Serial.print(F("Reading started at "));
-  Serial.print(millis());
-  Serial.print(F(" and will finish at "));
-  Serial.println(endTime);
-
-  Serial.println(F("You can do other work during BME680 measurement."));
-  delay(50); // This represents parallel work.
-  // There's no need to delay() until millis() >= endTime: bme.endReading()
-  // takes care of that. It's okay for parallel work to take longer than
-  // BME680's measurement time.
-
-  // Obtain measurement results from BME680. Note that this operation isn't
-  // instantaneous even if milli() >= endTime due to I2C/SPI latency.
-  if (!bme.endReading()) {
-    Serial.println(F("Failed to complete reading :("));
-    return;
-  }
-  Serial.print(F("Reading completed at "));
-  Serial.println(millis());
-
-  Serial.print(F("Temperature = "));
-  Serial.print(bme.temperature);
-  Serial.println(F(" *C"));
-
-  Serial.print(F("Pressure = "));
-  Serial.print(bme.pressure / 100.0);
-  Serial.println(F(" hPa"));
-
-  Serial.print(F("Humidity = "));
-  Serial.print(bme.humidity);
-  Serial.println(F(" %"));
-
-  Serial.print(F("Gas = "));
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(F(" KOhms"));
-
-  Serial.print(F("Approx. Altitude = "));
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(F(" m"));
-
-  Serial.println();
-  delay(2000);
+void loop()
+{
 }
